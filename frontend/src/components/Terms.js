b3e01e187ba1be5af5a6b1cd0607abccbe5dd646
jkarpiakk <jakub.karpiak@gmail.com>
2025-07-19 18:35:14 +0200

    Create Terms.js

---
+++ frontend/src/components/Terms.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Terms = () => {
+  return (
+    <div className="container mt-4">
+      <h1>Regulamin</h1>
+      <p>Zasady anonimowości, ochrona danych pacjentów, procedura moderacji.</p>
+      {/* Dodaj pełny tekst */}
+    </div>
+  );
+};
+
+export default Terms;
