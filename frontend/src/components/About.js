8ccfb90524ded5604190293b825dbaf4614aae90
jkarpiakk <jakub.karpiak@gmail.com>
2025-07-19 18:35:36 +0200

    Create About.js

---
+++ frontend/src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="container mt-4">
+      <h1>O nas</h1>
+      <p>Cel i pomysł serwisu, krótkie przedstawienie zespołu.</p>
+      {/* Dodaj pełny tekst */}
+    </div>
+  );
+};
+
+export default About;
